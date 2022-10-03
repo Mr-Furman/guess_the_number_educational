9511ab3af010db67246f9132d58f4796a2287ddf
Rostyslav Furman <106009301+Mr-Furman@users.noreply.github.com>
2022-10-03 11:46:19 +0300

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,3 +1,4 @@
+from asyncore import read
 import random
 
 number_which_we_search = random.randint(1, 100)
@@ -5,11 +6,13 @@ number_which_we_search = random.randint(1, 100)
 
 class Digit_for_game():
 
-    def __init__(self, number:int, life:int):
+    def __init__(self, number:int, life:int, name:str):
         self.number = number
         self.life = life
+        self.name = name
         
-        
+
+
     def guess_the_number(self) -> None:
         try:
             try:
@@ -33,6 +36,33 @@ class Digit_for_game():
             pass
 
 
-test = Digit_for_game(3, 7)
-test.guess_the_number()
+    def magic_layer(self) -> None:
+        list_of_answer = ['It is certain', 'It is decidedly so.', 'Without a doubt.', 'Yes definitely','You may rely on it.',
+                         'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.',
+                         'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now', 'Concentrate and ask again.',
+                         'Don\'t count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+        print(f"Hi {self.name}, I'm a magic layer and I know the answer to any question you have.")
+        while True:
+            quest_for_answer = input('Ask a question:  ')
+            print(random.choice(list_of_answer))
+            repeat_1 = input('Do you have a one more quest?(+ or -)\n',)
+            if repeat_1 == '+':
+                continue
+            elif repeat_1 == '-':
+                print('Good luck')
+                return False
+            elif repeat_1 != '+' or repeat_1 != '-':
+                print('OMG I need + or -')
+                print('Bye Bye')
+                
+            if quest_for_answer.lower() == 'exit' or repeat_1 == 'exit':
+                return False
+    
+
+
+   
+
+test = Digit_for_game(3, 7, 'Robert Polson')
+test.magic_layer()
+
 
